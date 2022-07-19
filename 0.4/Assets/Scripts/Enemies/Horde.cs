33333b3d8d337a26c83456fde24ed9b8f5d20864
AenArMarie <vafailgvinbleid@gmail.com>
2022-07-19 22:56:21 +0400

    Пофиксил AI

--- 0.4/Assets/Scripts/Enemies/Horde.cs
+++ 0.4/Assets/Scripts/Enemies/Horde.cs
@@ -1,12 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Horde : MonoBehaviour
 {
+    private int number = 0;
+    private int current = 0;
     [Header("Enemies")]
     public Skeleton[] skeletons;
-
+    public void Start()
+    {
+        foreach (var skeleton in skeletons)
+        {
+            number++;
+        }
+        foreach (var skeleton in skeletons)
+        {
+            skeleton.radiuscos = (float)(1.5 * Math.Cos(2*Math.PI*current/number));
+            skeleton.radiussin = (float)(1.5 * Math.Sin(2 * Math.PI * current / number));
+            current++;
+        }
+    }
     public void Alert()
     {
         foreach (var skeleton in skeletons)
--- 0.4/Assets/Scripts/Enemies/Skeleton.cs
+++ 0.4/Assets/Scripts/Enemies/Skeleton.cs
@@ -10,9 +10,13 @@ public class Skeleton : MonoBehaviour
     public Horde horde;
     public Animator animator;
     private Vector3 lastpos;
+    private Vector3 diff;
+    private Vector3 idlecheck;
     private float updateTime = 0;
     public bool alerted = false;
     public float check;
+    public float radiuscos=0;
+    public float radiussin=0;
     void Start()
     {
         animator.SetBool("Idle", true);
@@ -21,9 +25,10 @@ public class Skeleton : MonoBehaviour
     }
     public void Update()
     {
+        idlecheck = transform.position - lastpos;
         updateTime += Time.deltaTime;
         float distance = Vector3.Distance(this.transform.position, player.transform.position);
-        check = distance;
+        check = idlecheck.magnitude;
         if ((distance <=10.0)&&(!alerted))
         {
                 horde.Alert();
@@ -35,6 +40,7 @@ public class Skeleton : MonoBehaviour
             {
                 animator.SetBool("Walking", false);
                 animator.SetBool("Attack", true);
+                transform.LookAt(player.transform);
             }
             else if (distance > 20.0)
             {
@@ -47,7 +53,7 @@ public class Skeleton : MonoBehaviour
                 animator.SetBool("Walking", true);
             }
         }
-        if (!alerted && lastpos.Equals(this.transform.position))
+        if (!alerted && idlecheck.magnitude<2)
         {
             animator.SetBool("Walking", false);
             animator.SetBool("Idle", true);
@@ -59,7 +65,11 @@ public class Skeleton : MonoBehaviour
         if (updateTime > 1)
         {
             if (alerted)
-                nav.destination = player.transform.position;
+            {
+                diff = new Vector3(player.transform.position.x + radiuscos, player.transform.position.y, player.transform.position.z +radiussin);
+                nav.destination = diff;
+            }
+                
             updateTime = 0;
         }
     }
