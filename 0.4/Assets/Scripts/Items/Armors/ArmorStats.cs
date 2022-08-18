9af5313916f41b5a39c2b7a68110d92bc850990f
njkz567 <79510562+njkz567@users.noreply.github.com>
2022-08-18 11:53:56 +0300

    Ассеты брони

--- 0.4/Assets/Scripts/Items/Armors/ArmorStats.cs
+++ 0.4/Assets/Scripts/Items/Armors/ArmorStats.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Object/Item/Armor")]
 public class ArmorStats : ObjectData
 {
+    public ArmorType armorType;
     public int armorPoints;
 }
